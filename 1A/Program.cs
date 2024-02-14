1e2343b94a7d54b8f7eb8939be3502b0f19b6a92
tamjid <tamjidakash100@gmail.com>
2024-02-14 21:48:22 +0600

    Solved 1A

---
+++ 1A/Program.cs
@@ -0,0 +1,12 @@
+﻿var input = Console.ReadLine().Split();
+
+long n = Int64.Parse(input[0]);
+long m = Int64.Parse(input[1]);
+long a = Int64.Parse(input[2]);
+
+long x = n / a; 
+long y = m/a;
+if (n % a != 0)    x++;
+
+if (m % a != 0)    y++;
+Console.WriteLine(x*y);
