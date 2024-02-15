d34f6a6957139f7c3ac615613f4e86235bd2f57c
tamjid <tamjidakash100@gmail.com>
2024-02-15 20:38:20 +0600

    Solved 158A and 282A

---
+++ 158A/Program.cs
@@ -0,0 +1,18 @@
+﻿var input = Console.ReadLine().Split();
+
+int n = Int32.Parse(input[0]);
+int m = Int32.Parse(input[1]);
+
+var a = Console.ReadLine().Split();
+int count = 0;
+
+for (int i = 0; i < n; i++)
+{
+    if (Int32.Parse(a[i]) == 0) break;
+    if (Int32.Parse(a[i]) >= Int32.Parse(a[m-1]))
+    {
+        count++;
+    }
+    else break;
+}
+Console.WriteLine(count);
---
+++ 282A/Program.cs
@@ -0,0 +1,13 @@
+﻿int input = Int32.Parse(Console.ReadLine());
+int count = 0;
+string a;
+for (int i = 0; i<input; i++)
+{
+    a = Console.ReadLine();
+    if (a == "X++" || a == "++X")
+    {
+        count++;
+    }
+    else count--;
+}
+Console.WriteLine(count);
