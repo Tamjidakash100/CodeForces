3bf9269617e438bbe845d2d096aac33bbc5e2add
tamjid <tamjidakash100@gmail.com>
2024-02-18 00:05:58 +0600

    Solved 112A

---
+++ 112A/Program.cs
@@ -0,0 +1,21 @@
+﻿string a = Console.ReadLine();
+string b = Console.ReadLine();
+
+a = a.ToLower();
+b = b.ToLower();
+
+if (a == b) Console.WriteLine(0);
+
+else
+{
+    for (int i = 0;i< a.Length; i++)
+    {
+        if (a[i] > b[i])
+        {
+            Console.WriteLine(1); break;
+        }
+        else if (a[i] == b[i]) continue; 
+        else { Console.WriteLine(-1); break; }
+    }
+
+}
---
+++ 339A/Program.cs
@@ -0,0 +1,8 @@
+﻿var input = Console.ReadLine().Split("+");
+
+int[] ints = new int[input.Length];
+
+for (int i = 0; i < ints.Length; i++)
+{
+    ints[i] = int.Parse(input[i]);
+}
