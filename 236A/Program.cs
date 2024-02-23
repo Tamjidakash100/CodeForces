9107cf01c4443ea8f5dc9d5d96120bb938604206
tamjid <tamjidakash100@gmail.com>
2024-02-24 00:03:24 +0600

    solved 339A and 236A

---
+++ 236A/Program.cs
@@ -0,0 +1,10 @@
+﻿string input = Console.ReadLine();
+
+string a = "";
+
+for (int i = 0; i < input.Length; i++)
+{
+    if (!a.Contains(input[i])) a+= input[i];
+}
+if (a.Length % 2 == 0) Console.WriteLine("CHAT WITH HER!");
+else Console.WriteLine("IGNORE HIM!");
--- 339A/Program.cs
+++ 339A/Program.cs
@@ -6,3 +6,13 @@ for (int i = 0; i < ints.Length; i++)
 {
     ints[i] = int.Parse(input[i]);
 }
+Array.Sort(ints);
+for (int i = 0; i<ints.Length; i++)
+{
+    if (i == ints.Length - 1)
+    {
+        Console.WriteLine(ints[i]);
+        break;
+    }
+    Console.Write(ints[i]+"+");
+}
