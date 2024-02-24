e7404e45e7bee282419f7d6c9e16ee5092d34dd7
tamjid <tamjidakash100@gmail.com>
2024-02-25 01:02:45 +0600

    solved 118A and 266A

---
+++ 118A/Program.cs
@@ -0,0 +1,10 @@
+﻿string input = Console.ReadLine();
+input = input.ToLower();
+for(int i=0; i<input.Length; i++)
+{
+    if (input[i] !=  'a' && input[i] != 'e' && input[i] != 'i' && input[i] != 'o' && input[i] != 'u' && input[i] != 'y')
+    {
+        Console.Write("." + input[i]);
+    }
+}
+Console.WriteLine();
---
+++ 266A/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.ReadLine();
+string input = Console.ReadLine();
+int count = 0;
+char prev = input[0];
+for(int i = 1; i < input.Length; i++)
+{
+    if (prev == input[i]) count++;
+    else prev = input[i];
+}
+Console.WriteLine(count);
