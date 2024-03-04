3be25d8e98d3e69c8aef9290ec97e6175622962c
tamjid <tamjidakash100@gmail.com>
2024-03-04 21:27:18 +0600

    Solved 59A and 546A

---
+++ 546A/Program.cs
@@ -0,0 +1,14 @@
+﻿var input = Console.ReadLine().Split();
+
+int k = int.Parse(input[0]);
+int n = int.Parse(input[1]);
+int w = int.Parse(input[2]);
+
+int m = 0;
+for (int i = 1; i <= w; i++)
+{
+    m += k* i;
+}
+if (m-n < 0) m = n;
+
+Console.WriteLine(m-n);
---
+++ 59A/Program.cs
@@ -0,0 +1,11 @@
+﻿var input = Console.ReadLine();
+int uCounter = 0, lCounter = 0;
+for (int i = 0; i<input.Length; i++)
+{
+    if (Char.IsUpper(input[i])) uCounter++;
+    if (Char.IsLower(input[i])) lCounter++;
+}
+if (uCounter>lCounter) input = input.ToUpper();
+else input =  input.ToLower();
+
+Console.WriteLine(input);
